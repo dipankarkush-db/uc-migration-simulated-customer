f5c0aeba87cf3908eb0e80a1de81f393801c159f
Dipankar Kushari <dipankar.kushari@databricks.com>
2023-08-29 18:46:44 +0000

    Changes from context manager

--- Meijer_Code_Simulator_Notebook_Example.py
+++ Meijer_Code_Simulator_Notebook_Example.py
@@ -4,18 +4,18 @@
 # COMMAND ----------
 
 entity_name = "people_parquet"
-stgloc = "/mnt/meijermount/dkushari/dkushari_db/ADRMParquet/{0}Stg".format(entity_name)
+stgloc = get_uc_mount_target('/mnt/meijermount', normalize=True) + "/dkushari/dkushari_db/ADRMParquet/{0}Stg".format(entity_name)
 display(spark.read.format("parquet").load(stgloc))
 
 # COMMAND ----------
 
 from pyspark.sql.functions import col
-peopleid = spark.read.parquet("/mnt/meijermount/dkushari/dkushari_db/ADRMParquet/people_parquetStg").filter(col("id")==6280266)
+peopleid = spark.read.parquet(get_uc_mount_target('/mnt/meijermount', normalize=True) + "/dkushari/dkushari_db/ADRMParquet/people_parquetStg").filter(col("id")==6280266)
 
 # COMMAND ----------
 
 table = "people_parquetStg2"
-tgtlocExt = "/mnt/meijermount/dkushari/dkushari_db/ADRMParquet/{0}".format(table)
+tgtlocExt = get_uc_mount_target('/mnt/meijermount', normalize=True) + "/dkushari/dkushari_db/ADRMParquet/{0}".format(table)
 tgtlocExt
 
 # COMMAND ----------
@@ -24,29 +24,29 @@ display(spark.read.parquet(tgtlocExt))
 
 # COMMAND ----------
 
-dbutils.fs.rm('/mnt/meijermount/dkushari/dkushari_db/ADRMParquet/people_parquetStg1',True);
+dbutils.fs.rm(get_uc_mount_target('/mnt/meijermount', normalize=True) + '/dkushari/dkushari_db/ADRMParquet/people_parquetStg1',True);
 
 # COMMAND ----------
 
-people = spark.read.parquet("/mnt/meijermount/dkushari/dkushari_db/ADRMParquet/people_parquetStg")
+people = spark.read.parquet(get_uc_mount_target('/mnt/meijermount', normalize=True) + "/dkushari/dkushari_db/ADRMParquet/people_parquetStg")
 display(people)
 
 # COMMAND ----------
 
 sourceSystem = "ADRMParquet"
 source_table_name = "people_parquetStg/part-00000-tid-1383576846752282503-c6933315-90e2-4a66-9cc2-8bf8d1a4c200-239-1-c000.snappy"
-metadata_filepath="/mnt/meijermount/dkushari/dkushari_db/{}/{}.parquet".format(sourceSystem,source_table_name)
+metadata_filepath=get_uc_mount_target('/mnt/meijermount', normalize=True) + "/dkushari/dkushari_db/{}/{}.parquet".format(sourceSystem,source_table_name)
 display(spark.read.parquet(metadata_filepath))
 
 # COMMAND ----------
 
-mountPoint = "/mnt/meijermount"
+mountPoint = get_uc_mount_target('/mnt/meijermount', normalize=True) + ""
 
 # COMMAND ----------
 
-clnsmountPoint = "/mnt/meijermount"
+clnsmountPoint = get_uc_mount_target('/mnt/meijermount', normalize=True) + ""
 
-list_of_mounts = ["/mnt/meijermount", "/mnt/meijermount2"]
+list_of_mounts = [get_uc_mount_target('/mnt/meijermount', normalize=True) + "", "/mnt/meijermount2"]
 
 # COMMAND ----------
 
